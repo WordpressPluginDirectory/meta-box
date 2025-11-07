eebf26250c90fdecf4f63734b6ba424533e3cddf
Justin Gardner <justin@rhynorater.com>
2025-11-07 06:03:04 -0500

    5.10.17

--- meta-box/vendor/composer/installed.php
+++ meta-box/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpmetabox/meta-box',
-        'pretty_version' => '5.10.16',
-        'version' => '5.10.16.0',
-        'reference' => '9f3598cfb7e2377eac5df557a7e84ee63a29c21d',
+        'pretty_version' => '5.10.17',
+        'version' => '5.10.17.0',
+        'reference' => 'd305020f9cd268c69642292972cffb769e6428f3',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpmetabox/meta-box' => array(
-            'pretty_version' => '5.10.16',
-            'version' => '5.10.16.0',
-            'reference' => '9f3598cfb7e2377eac5df557a7e84ee63a29c21d',
+            'pretty_version' => '5.10.17',
+            'version' => '5.10.17.0',
+            'reference' => 'd305020f9cd268c69642292972cffb769e6428f3',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
